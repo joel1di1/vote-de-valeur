715297ebbd77e421a50abdf2585079356ec987be
Benoit Lafontaine <joel1di1@gmail.com>
2011-06-06 01:22:24 +0200

    sign in header

--- test/integration/home_ui_test.rb
+++ test/integration/home_ui_test.rb
@@ -2,13 +2,27 @@ require "integration_test_helper"
 
 class HomeUiTest < ActionDispatch::IntegrationTest
 
+  setup do
+    ui_sign_out
+  end
+
   test "home should say vote de valeur" do
     visit '/'
     assert page.has_content? 'Vote De Valeur'
   end
 
-  test "user sign in" do
-    User.create
+  test "home should display sign in" do
     visit '/'
+    assert page.has_content? "sign in"
+  end
+
+
+  test "user sign in test" do
+    assert !page.has_content?("signed in as")
+
+    user = Factory :user
+    ui_sign_in user
+    puts page.body
+    assert page.has_content?("signed in as #{user.email}")
   end
 end
