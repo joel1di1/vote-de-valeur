7b78018304f426d93fc2745563f21e4b8d5a603a
Benoit Lafontaine <joel1di1@gmail.com>
2011-06-05 22:47:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoteDeValeur::Application.config.secret_token = '17bea8886a36190189ff3e75666253a14f6bc05b75464e5af424e21cd9bb35f5c7c2bf60410fc8fc09086eaa1f2e2b45a7b20257966469401524a8d57f29f08b'
