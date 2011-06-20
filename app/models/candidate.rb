ae0d4cc3075fed1957707cbcbab7742052551a6c
Benoit Lafontaine <joel1di1@gmail.com>
2011-06-20 23:44:04 +0200

    fix bug on sum candidate with nil vote

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,7 +2,9 @@ class Candidate < ActiveRecord::Base
   has_many :votes
   def votes_total
     total = 0
-    votes.each{|v| total = total + v.vote}
+    votes.each{|v|
+      total = total + v.vote unless v.vote.nil?
+    }
     total
   end
 end
--- test/unit/candidate_test.rb
+++ test/unit/candidate_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "addition" do
+    candidate = Factory :candidate
+    user = Factory :user
+    vote = Factory :vote, :user => user, :candidate => candidate, :vote => nil
+
+    candidate.reload
+
+    assert_equal 0, candidate.votes_total
+
   end
 end
