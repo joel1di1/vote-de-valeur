1fe3e54a5357733d621951765c93b06c5b532071
Benoit Lafontaine <joel1di1@gmail.com>
2011-06-26 23:46:50 +0200

    style sur les votes classique et valeur

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,31 +1,33 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
+// Lorsqu'une valeur est choisie, on affiche la selection
 $(function(){
-    $("#user_votes input[checked='checked']").each(function (){
-        highlight_label(this.id)
+    $("input[type='radio']").change(function(){
+        $("input[name='"+this.name+"']")
+            .each(function (){
+                unhighlight_label(this.id)
+            })
+            .filter("[checked]")
+            .each(function (){
+                highlight_label(this.id)
+            })
     })
 })
-
-
-$(function(){
-    $("#user_votes input[type='radio']").change(function(){
-        $("#user_votes input[name='"+this.name+"']")
-                .each(function (){
-                    unhighlight_label(this.id)
-                })
-                .filter("[checked]")
-                .each(function (){
-                    highlight_label(this.id)
-                })
-                .end()
-    })
-})
-
 
 function highlight_label(for_s){
-    $("label[for='"+for_s+"']").addClass("selected")
+    $("label[for='"+for_s+"']")
+        .addClass("selected")
+        .removeClass("unselected")
 }
 function unhighlight_label(for_s){
-    $("label[for='"+for_s+"']").removeClass("selected")
+    $("label[for='"+for_s+"']")
+        .addClass("unselected")
+        .removeClass("selected")
 }
+
+// transformer les radios du vote classique
+$(function(){
+    $("label.collection_radio").addClass('unselected')
+    $("input[type='radio']").addClass('hidden')
+        .filter("[checked]").each(function(){
+            highlight_label(this.id)
+        })
+})
