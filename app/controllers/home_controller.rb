1fe992049348bb5bb0eb6cbbde99e09a6c400ece
Benoit Lafontaine <joel1di1@gmail.com>
2011-07-06 02:34:11 +0200

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,10 @@ class HomeController < ApplicationController
   end
 
   def add_user
+    h = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PWD'])
     h.set_workers("evening-moon-670", h.workers("evening-moon-670")+1)
     User.delay.add_fake
     render :json => {'res' => 'OK'}
-    h = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PWD'])
   end
 
 end
