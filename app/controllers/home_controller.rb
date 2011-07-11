7e1ae214824911ea17a26fb9d9cafd70cb16a9e2
Benoit Lafontaine <joel1di1@gmail.com>
2011-07-11 02:35:40 +0200

    corrections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,15 +7,4 @@ class HomeController < ApplicationController
       @user =  User.new
     end
   end
-
-  def add_user
-    if Rails.env.production? && (ENV['workers']=='auto')
-      h = Heroku::Client.new(ENV['HEROKU_USER'], ENV['HEROKU_PWD'])
-      h.set_workers("evening-moon-670", h.workers("evening-moon-670")+1)
-    end
-
-    User.delay.add_fake
-    render :json => {'res' => 'OK'}
-  end
-
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,47 +2,54 @@
 class VotesController < ApplicationController
 
   TOKEN_VALIDATED_KEY = 'token_validated'
-  before_filter :authenticate_user!
 
   def index
-    if DateHelper.election_running?
-      if session[TOKEN_VALIDATED_KEY]
-        @candidates = Candidate.all
-        @user = current_user
+    if user_signed_in?
+      if DateHelper.election_running?
+        if session.include? TOKEN_VALIDATED_KEY && session[TOKEN_VALIDATED_KEY]
+          @candidates = Candidate.all
+          @user = current_user
 
-        respond_to do |format|
-          format.html
+          respond_to do |format|
+            format.html
+          end
+        else
+          flash[:error] = "Veuillez suivre le lien qui vous à été envoyé par mail."
+          redirect_to root_path
         end
       else
-        flash[:error] = "Veuillez suivre le lien qui vous à été envoyé par mail."
+        flash[:error] = "Le bureau de vote n'est pas ouvert"
         redirect_to root_path
       end
     else
-      flash[:error] = "Le bureau de vote n'est pas ouvert"
       redirect_to root_path
     end
   end
 
   def update
-    if DateHelper.election_running?
-      current_user.votes.clear
+    if user_signed_in?
+      if DateHelper.election_running?
+        current_user.votes.clear
 
-      Candidate.all.each do |candidate|
-        value_string = params[:user]["vote_for_candidate_#{candidate.id}"]
+        Candidate.all.each do |candidate|
+          value_string = params[:user]["vote_for_candidate_#{candidate.id}"]
 
-        vote_value = parse_vote_value value_string
+          vote_value = parse_vote_value value_string
 
-        vote = Vote.new :user => current_user, :candidate => candidate, :vote => vote_value
-        current_user.votes << vote
+          vote = Vote.new :user => current_user, :candidate => candidate, :vote => vote_value
+          current_user.votes << vote
+        end
+
+        current_user.classic_vote = ClassicVote.create :user => current_user unless current_user.classic_vote
+        current_user.classic_vote.candidate = Candidate.find(params[:user][:classic_vote])
+        current_user.classic_vote.save
+
+        redirect_to votes_url, :notice => "modifications prise en compte"
+      else
+        flash[:error] = "Le bureau de vote n'est pas ouvert"
+        redirect_to root_path
       end
-
-      current_user.classic_vote = ClassicVote.create :user => current_user unless current_user.classic_vote
-      current_user.classic_vote.candidate = Candidate.find(params[:user][:classic_vote])
-      current_user.classic_vote.save
-
-      redirect_to votes_url, :notice => "modifications prise en compte"
     else
-      flash[:error] = "Le bureau de vote n'est pas ouvert"
       redirect_to root_path
     end
   end
