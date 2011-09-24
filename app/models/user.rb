940c93c8af3a77d01576e1eb37023a68d3c27e79
Benoit Lafontaine <joel1di1@gmail.com>
2011-09-24 10:54:36 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ end
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :trackable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :postal_code, :public
