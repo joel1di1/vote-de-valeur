c02e5088c9ff458d6f669730bd7991404c8bcb32
Benoit Lafontaine <joel1di1@gmail.com>
2011-10-23 14:05:32 +0200

    add sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,16 @@ VoteDeValeur::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
 end
