69687cbacef09c62cbbe536d9b653db018ed9af8
unknown <Jerome@.(none)>
2012-02-05 12:17:13 +0100

    modif mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 # coding: utf-8
 class UserMailer < ActionMailer::Base
-  default :from => "no-reply@votedevaleur.org"
+  default :from => "Vote de Valeur, expérience 2012 <contact@votedevaleur.org>"
 
   def send_confirmation user
     @user = user
-    mail(:to => user.email, :subject => "Bienvenue dans l'expérimentation du vote de valeur")
+    mail(:to => user.email, :subject => "Bienvenue dans l'expérimentation du Vote de Valeur")
   end
 
   def election_open_mail user
     @user = user
-    mail(:to => user.email, :subject => "Le vote de valeur est ouvert")
+    mail(:to => user.email, :subject => "Le Vote de Valeur est ouvert")
   end
 end
