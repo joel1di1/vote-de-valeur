9fc42c5a7dd44d3dbb1441b2813130a7fcf9f033
Benoit Lafontaine <joel1di1@gmail.com>
2012-04-08 20:41:20 +0200

    use session in DB

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-VoteDeValeur::Application.config.session_store :cookie_store, :key => '_vote-de-valeur_session'
+VoteDeValeur::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
