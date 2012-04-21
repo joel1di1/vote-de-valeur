fd4ce8d03835e50ad8a5a5f3d4f520bec10a470e
Benoit Lafontaine <joel1di1@gmail.com>
2012-04-22 01:21:06 +0200

    try something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_filter :is_demo, :only => [:opening_email, :send_opening_email]
   before_filter :redirect_when_closed, :access
+  before_filter :set_cache_buster  
 
   def access
     token = params[:id]
