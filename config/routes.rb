43df487db995bcf3e2a44c5354d5c2e5c704efe2
Benoit Lafontaine <joel1di1@gmail.com>
2012-04-24 20:42:27 +0200

    revert mail subscription

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 VoteDeValeur::Application.routes.draw do
 
   # # very custom devise
-  # devise_for :users, :only => [] do
-  #   post  '/users'                     =>  'devise/registrations#create', :as => :user_registration
-  #   get   '/users/sign_up'             =>  'devise/registrations#new'   , :as => :new_user_registration
-  #   get   '/users/sign_out'            =>  'devise/sessions#destroy'    , :as => :destroy_user_session
-  #   get   '/users/resend_instructions' => 'home#resend_instructions'
-  #   post  '/users/resend_instructions' => 'home#do_resend_instructions'
-  # end
+  devise_for :users, :only => [] do
+    post  '/users'                     =>  'devise/registrations#create', :as => :user_registration
+    get   '/users/sign_up'             =>  'devise/registrations#new'   , :as => :new_user_registration
+    get   '/users/sign_out'            =>  'devise/sessions#destroy'    , :as => :destroy_user_session
+    get   '/users/resend_instructions' => 'home#resend_instructions'
+    post  '/users/resend_instructions' => 'home#do_resend_instructions'
+  end
 
-  # resources :users, :only => [] do
-  #   collection do 
-  #     get 'count'
-  #   end
-  # end
+  resources :users, :only => [] do
+    collection do 
+      get 'count'
+    end
+  end
   # constraints :id => /[^\/]+/ do
   #   get 'users/opening_email'
   #   post 'users/send_opening_email' => 'users#send_opening_email'
@@ -32,13 +32,11 @@ VoteDeValeur::Application.routes.draw do
   # get 'feedbacks' => 'feedbacks#new'
   # post 'feedbacks' => 'feedbacks#create'
 
-  # get 'thanks' => 'home#thanks'
+  get 'thanks' => 'home#thanks'
 
-  # get 'elections_closed' => 'home#elections_closed'
-
-  # get 'users/access/:id'  =>  'users#access', :as => 'user_access'
+  get 'elections_closed' => 'home#elections_closed'
 
   get 'users/access/:id'  =>  redirect('/')
 
-  # root :to => "home#index"
+  root :to => "home#index"
 end
